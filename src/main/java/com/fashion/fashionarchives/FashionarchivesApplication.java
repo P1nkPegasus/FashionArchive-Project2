6f14fa3e1c934c76b2b03a6fadd091df8b9ec987
pinkpegasus <rukiyadykes@gmail.com>
2022-01-31 11:31:30 -0600

    created springboot

---
+++ src/main/java/com/fashion/fashionarchives/FashionarchivesApplication.java
@@ -0,0 +1,13 @@
+package com.fashion.fashionarchives;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FashionarchivesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FashionarchivesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/fashion/fashionarchives/FashionarchivesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fashion.fashionarchives;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FashionarchivesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
