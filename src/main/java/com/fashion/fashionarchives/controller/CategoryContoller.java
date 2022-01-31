28808e7ca6b28994c10fb46787691996edfe2683
pinkpegasus <rukiyadykes@gmail.com>
2022-01-31 11:38:48 -0600

    created categoryController

---
+++ src/main/java/com/fashion/fashionarchives/controller/CategoryContoller.java
@@ -0,0 +1,19 @@
+package com.fashion.fashionarchives.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+public class CategoryContoller {
+    @RestController
+    @RequestMapping(path = "/api")
+    public class CategoryController {
+
+
+        private CategoryService categoryService;
+
+        @Autowired
+        public void setCategoryService (CategoryService categoryService) {
+            this.categoryService = categoryService;
+        }
+}
