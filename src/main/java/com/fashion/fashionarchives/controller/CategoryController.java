f1e09d0cbe09f2d8c255302dde73ecada86b71e2
pinkpegasus <rukiyadykes@gmail.com>
2022-02-01 12:29:14 -0600

    created exceptions package and InformationExistException class

--- src/main/java/com/fashion/fashionarchives/controller/CategoryController.java
+++ src/main/java/com/fashion/fashionarchives/controller/CategoryController.java
@@ -2,20 +2,32 @@ package com.fashion.fashionarchives.controller;
 
 import com.fashion.fashionarchives.service.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 
-    @RestController
+@RestController
     @RequestMapping(path = "/api")
     public class CategoryController {
 
 
         private CategoryService categoryService;
 
-//        @Autowired
-//        public void setCategoryService(CategoryService categoryService) {
-//            this.categoryService = categoryService;
-//        }
+    @Autowired
+    public void setCategoryService (CategoryService categoryService) {
+        this.categoryService = categoryService;
+    }
+
+
+
+    @GetMapping(path = "/hello-world/")
+    public String getHelloWorld(){
+        return "hello world";
+    }
+
+
+
     }
 
---
+++ src/main/java/com/fashion/fashionarchives/exceptions/InformationExistException.java
@@ -0,0 +1,4 @@
+package com.fashion.fashionarchives.exceptions;
+
+public class InformationExistException {
+}
