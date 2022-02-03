ae4e8200be439cb335aa257598efa95c1a13dce1
pinkpegasus <rukiyadykes@gmail.com>
2022-02-03 16:26:37 -0600

    updated Piece class to join with designer table

--- src/main/java/com/fashion/fashionarchives/model/Piece.java
+++ src/main/java/com/fashion/fashionarchives/model/Piece.java
@@ -1,6 +1,8 @@
 package com.fashion.fashionarchives.model;
 
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import javax.persistence.*;
 
 @Entity
@@ -10,7 +12,7 @@ public class Piece {
     @Id
     @Column
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long pieceId;
+    private Long id;
 
     @Column
     private String color;
@@ -21,22 +23,38 @@ public class Piece {
     @Column
     private String type;
 
-    public Piece(Long pieceId, String color, String textiles, String type) {
-        this.pieceId = pieceId;
+
+    public Designer getDesigner() {
+        return designer;
+    }
+
+
+    public void setDesigner(Designer designer) {
+        this.designer = designer;
+    }
+
+    @JsonIgnore
+    @ManyToOne
+    @JoinColumn(name = "designerId") // creating foreign key
+
+    private Designer designer;
+
+    public Piece() {
+    }
+
+    public Piece(Long id, String color, String textiles, String type) {
+        this.id = id;
         this.color = color;
         this.textiles = textiles;
         this.type = type;
     }
 
-    public Piece() {
+    public Long getId() {
+        return id;
     }
 
-    public Long getPieceId() {
-        return pieceId;
-    }
-
-    public void setPieceId(Long pieceId) {
-        this.pieceId = pieceId;
+    public void setId(Long id) {
+        this.id = id;
     }
 
     public String getColor() {
@@ -61,5 +79,16 @@ public class Piece {
 
     public void setType(String type) {
         this.type = type;
+    }
+
+    @Override
+    public String toString() {
+        return "Piece{" +
+                "id=" + id +
+                ", color='" + color + '\'' +
+                ", textiles='" + textiles + '\'' +
+                ", type='" + type + '\'' +
+                ", designer=" + designer +
+                '}';
     }
 }
